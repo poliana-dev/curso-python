59e6438ee4c7c7f6e434517ba502a3d0dbf8d333
poliana-dev <m.polisys02@gmail.com>
2025-01-15 14:49:22 -0300

    feat: tabuada e conversor de moeda 🥸

---
+++ exercicies/ex008.py
@@ -0,0 +1,7 @@
+# tabuada de um numero
+
+number = int(input('Digite um número para ver sua tabuada: '))
+
+for i in range (1,11):
+    tabua = number * i 
+    print(f"{number} x {i} = {tabua} ") 
---
+++ exercicies/ex009.py
@@ -0,0 +1,5 @@
+# conversor de moeda BRL > USD
+
+cash = float(input("Quantidade presente em sua carteira? R$ "))
+dollar = cash/6.05
+print(f"Com R${cash}, você pode obter US${dollar:.2f}")
